dadda6e5e96376ba1286536376ceab98842c9e6b
Ivan Shulga <shg.ivan97@gmail.com>
2020-09-07 01:44:11 +0300

    create product page

--- js/main.js
+++ js/main.js
@@ -9,10 +9,19 @@ $(".first-slide-slider").owlCarousel({
     lazyLoad:true
 });
 
+$(".product-table--img").owlCarousel({
+    nav: true,
+    navContainer: "#tableNavWrapper",
+    dots: false,
+    items: 1,
+    loop:true,
+    lazyLoad:true
+});
+
 $(".slider-wrapper").owlCarousel({
     nav: true,
     navContainer: "#sliderNavWrapper",
-    dots: false,
+    dots: true,
     loop:true,
     addClassActive: true,
     lazyLoad:true,
@@ -35,4 +44,41 @@ $(".burger").click(function(e) {
 
 $(function(){
     $("#numbers").mask("+7 (999) 999-99-99", {placeholder: " "});
+});
+
+$(function(){
+    $("#numbersPopup").mask("+7 (999) 999-99-99", {placeholder: " "});
+});
+
+$('.productSlide').on('click', function(e){
+    e.preventDefault();
+    $('html,body').stop().animate({ scrollTop: $('#product').offset().top }, 1000);
+    $(".burger").removeClass('activeBurger')
+    $('.header-right').removeClass('activeNav')
+});
+
+$('.faqSlide').on('click', function(e){
+    e.preventDefault();
+    $('html,body').stop().animate({ scrollTop: $('#faq').offset().top }, 1000);
+    $(".burger").removeClass('activeBurger')
+    $('.header-right').removeClass('activeNav')
+});
+
+$('.contactSlide').on('click', function(e){
+    e.preventDefault();
+    $('html,body').stop().animate({ scrollTop: $('#contact').offset().top }, 1000);
+    $(".burger").removeClass('activeBurger')
+    $('.header-right').removeClass('activeNav')
+});
+
+$(".popupOpen").click(function(){
+    $(".popup, .popup-fon").show();
+});
+
+$(".popup-close").click(function(){
+    $(".popup, .popup-fon").hide();
+});
+
+$(".popup-fon").click(function(){
+    $(".popup, .popup-thanks, .popup-fon").hide();
 });
